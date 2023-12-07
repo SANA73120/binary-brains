96f73bccf58657a64cc25b5ac266105d8696be28
Sana Khan <142918622+SANA73120@users.noreply.github.com>
2023-12-07 18:15:25 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d",&a,&b,&c);
+    printf("Average is %d",(a+b+c)/3);
+    getch();
+}
