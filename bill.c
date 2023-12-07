837e433f533448a2effa0d81489604180b55d3db
Sana Khan <142918622+SANA73120@users.noreply.github.com>
2023-12-07 18:30:15 +0530

    Create bill.c

---
+++ bill.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float a,b,c,gst,d;
+    printf("Enter cost of pencil: ");
+    scanf("%f",&a);
+    printf("Enter cost of pen: ");
+    scanf("%f",&b);
+    printf("Enter cost of eraser: ");
+    scanf("%f",&c);
+    d=a+b+c;
+    gst=(d*18)/100;
+    printf("%f",d+gst);
+    getch();
+}
